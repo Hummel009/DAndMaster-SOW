0414918adedacc5ea8990023880e2207315f0ab3
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-08-01 20:58:54 +0300

    1

--- src/main/java/sow/content/AWItemEmpty.java
+++
@@ -1,9 +0,0 @@
-package sow.content;
-
-import net.minecraft.world.item.Item;
-
-public class AWItemEmpty extends Item {
-	public AWItemEmpty() {
-		super(new Properties().stacksTo(1).tab(AWCreativeTabs.TAB_SOW));
-	}
-}
--- src/main/java/sow/proxy/AWCommonProxy.java
+++
@@ -1,4 +0,0 @@
-package sow.proxy;
-
-public class AWCommonProxy {
-}
